5c2a2b1409d667d346c2ac50e9cecef7ab3dff4f
jungseungjin <ywack@naver.com>
2022-11-16 21:27:09 +0900

    fix index.js add  return res.json

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import daangn from './function/daangn.js'
+import joongna from './function/joongna.js'
+import bunjang from './function/bunjang.js'
 
 const app = express()
 
@@ -11,10 +13,14 @@ app.get("/",(req,res)=>{
 app.get("/:item",async(req,res)=>{
     try{
         const item = req.params.item
-        const [daangn_data] =  await Promise.all([
-            daangn(item,5)
+        const [daangn_data,joongna_data,bunjang_data] =  await Promise.all([
+            daangn(item,5),//더보기를 몇번 누를 것인지
+            joongna(item,2),//몇 페이지까지 정보를 가져올 것인지
+            bunjang(item,2)
         ])
-        console.log(daangn_data)
+        return res.json({
+            daangn_data,joongna_data,bunjang_data
+        })
     }catch(err){
         console.log(err)
     }
